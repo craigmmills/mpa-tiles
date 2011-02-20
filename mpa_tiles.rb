eca705d826f02361c077f1646b693b1f448f45a7
Craig Mills <Craig.mills@unep-wcmc.org>
2011-02-20 20:30:31 +0000

    sorted weird mapbox y coordinate flipping...

--- mpa_tiles.rb
+++ mpa_tiles.rb
@@ -2,21 +2,26 @@ require 'sinatra'
 require 'sqlite3'
 require 'haml'
 
-get '/:zoom/:row/:column' do
+get '/tiles/:zoom/:column/:row' do
   content_type 'image/png'
+  
+  # flip round the y coordinate for mapbox
+  y = ((2**params[:zoom].to_f) - 1) - params[:row].to_f
+  
+  
   db = SQLite3::Database.new "data/wdpa_test_z5to8.mbtiles"
   rows = db.execute("select images.tile_data 
                      from map 
                      inner join images 
                      on (map.tile_id = images.tile_id) 
                      where zoom_level = #{params[:zoom]} AND 
-                     tile_row = #{params[:row]} AND 
+                     tile_row = #{y} AND 
                      tile_column = #{params[:column]}")
   
-  rows[0][0]
+  rows[0][0] unless rows.length == 0
 end
 
-get '/map' do
+get '/' do
   haml :index
 end
 
